ab797967f6ece11ef90f62cebabbcb98f7c896f0
laboratorio <laboratorio@UPOAULA10305>
2024-11-12 09:00:10 -0500

    Agregar archivos de proyecto.

---
+++ MauiJustin/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiJustin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiJustin/AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MauiJustin
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+}
